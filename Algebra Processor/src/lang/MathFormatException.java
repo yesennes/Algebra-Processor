95c1f1d034e15eea9f078afe3727b19d2021c544
Nikola Istvanic <istvanic.nikola@gmail.com>
2015-12-14 10:06:33 -0500

    Finished MathFormatException.java

--- Algebra Processor/src/lang/MathFormatException.java
+++ Algebra Processor/src/lang/MathFormatException.java
@@ -2,43 +2,40 @@ package lang;
 
 /**
  * Thrown when a String was not properly formated for whichever class attempted to parse it.
- * @author Luke Senseney
+ * 
+ * @author Luke Senseney, Nikola Istvanic
  */
-public class MathFormatException extends IllegalArgumentException
-{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID=1L;
+public class MathFormatException extends IllegalArgumentException {
+	private static final long serialVersionUID = 1L;
 
 	/**
-	 * 
+	 * Constructor for MathFormatException that takes no parameters.
 	 */
-	public MathFormatException()
-	{}
+	public MathFormatException() {
+	}
 
 	/**
-	 * @param message
+	 * Message of this exception.
+	 * @param message The exception's message.
 	 */
-	public MathFormatException(String message)
-	{
+	public MathFormatException(String message) {
 		super(message);
 	}
 
 	/**
-	 * @param cause
+	 * Constructor for MathFormatException which takes a throwable cause.
+	 * @param cause Throwable which is thrown with the exception.
 	 */
-	public MathFormatException(Throwable cause)
-	{
+	public MathFormatException(Throwable cause) {
 		super(cause);
 	}
 
 	/**
-	 * @param message
-	 * @param cause
+	 * Constructor for MathFormatException which takes in a String and Throwable.
+	 * @param message The exception's message.
+	 * @param cause Throwable thrown along with the exception.
 	 */
-	public MathFormatException(String message,Throwable cause)
-	{
-		super(message,cause);
+	public MathFormatException(String message, Throwable cause) {
+		super(message, cause);
 	}
 }
