4e4b713f1fce43a0bc03ce0004316f4227509be9
Nikola Istvanic <istvanic.nikola@gmail.com>
2015-12-14 10:17:10 -0500

    Finished NotEquation.java

--- Algebra Processor/src/lang/NotEquation.java
+++ Algebra Processor/src/lang/NotEquation.java
@@ -1,23 +1,25 @@
 package lang;
 
 /**
- * Thrown by Expression when a action is called on a non-equation that is only for equations.
- * @author Luke Senseney
+ * Thrown by Expression when an action is called on a non-equation that is only for equations.
+ * 
+ * @author Luke Senseney, Nikola Istvanic
  */
-public class NotEquation extends UnsupportedOperationException
-{
-	private static final long serialVersionUID=1L;
+public class NotEquation extends UnsupportedOperationException {
+	private static final long serialVersionUID = 1L;
 
-	public NotEquation()
-	{
+	/**
+	 * NotEquation exception constructor which takes in no parameters.
+	 */
+	public NotEquation() {
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see java.lang.Throwable#getMessage()
 	 */
-	@Override public String getMessage()
-	{
+	@Override
+	public String getMessage() {
 		return "This is not an equation and cannot be solved";
 	}
 }
