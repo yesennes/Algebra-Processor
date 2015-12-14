d1d5b8dd9df7559cfdf9ef5ad7d8faeff83105d4
Nikola Istvanic <istvanic.nikola@gmail.com>
2015-12-13 23:00:14 -0500

    Finally figured out Git

--- Algebra Processor/src/lang/DifferentRoots.java
+++ Algebra Processor/src/lang/DifferentRoots.java
@@ -1,15 +1,13 @@
 package lang;
 
-public class DifferentRoots extends RuntimeException
-{
-	private static final long serialVersionUID=1L;
+public class DifferentRoots extends RuntimeException {
+	private static final long serialVersionUID = 1L;
 
-	public DifferentRoots()
-	{
+	public DifferentRoots() {
 	}
 
-	@Override public String getMessage()
-	{
+	@Override
+    public String getMessage() {
 		return "The Constants being added together had different roots and cannot be";
 	}
 }
