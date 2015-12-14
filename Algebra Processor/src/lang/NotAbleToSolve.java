f705e4a7891a1b8fa57fb7c4915d9e30d7ad82cc
Nikola Istvanic <istvanic.nikola@gmail.com>
2015-12-14 10:15:16 -0500

    Finished NotAbleToSolve.java

--- Algebra Processor/src/lang/NotAbleToSolve.java
+++ Algebra Processor/src/lang/NotAbleToSolve.java
@@ -1,24 +1,28 @@
 package lang;
 
 /**
- * Thrown when Expression tried to solve itself and couldn't.
- * @author Luke Senseney
+ * Thrown when an Expression tried to solve itself but couldn't.
+ * 
+ * @author Luke Senseney, Nikola Istvanic
  */
-public class NotAbleToSolve extends Exception
-{
-	private static final long serialVersionUID=1L;
+public class NotAbleToSolve extends Exception {
+	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @param message 
+	 * Constructor for NotAbleToSolve exception which has a String parameter.
+	 * @param message The exception's message.
 	 */
-	public NotAbleToSolve(String message)
-	{
+	public NotAbleToSolve(String message) {
 		super(message);
 		// TODO Auto-generated constructor stub
 	}
 	
-	public NotAbleToSolve(String message, Throwable cause)
-	{
-		super(message,cause);
+	/**
+	 * NotAbleToSolve constructor that takes both a String and a Throwable.
+	 * @param message The exception's message.
+	 * @param cause The cause of the exception.
+	 */
+	public NotAbleToSolve(String message, Throwable cause) {
+		super(message, cause);
 	}
-}// Glory to God
+}
