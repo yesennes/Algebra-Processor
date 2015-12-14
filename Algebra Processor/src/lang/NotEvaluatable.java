b8dd0d4d436bc8918f522b13662ea7d0e5b6be8b
Nikola Istvanic <istvanic.nikola@gmail.com>
2015-12-14 10:18:40 -0500

    Finished NotEvaluatable.java

--- Algebra Processor/src/lang/NotEvaluatable.java
+++ Algebra Processor/src/lang/NotEvaluatable.java
@@ -1,30 +1,53 @@
 package lang;
 
-public class NotEvaluatable extends Exception
-{
-	private static final long serialVersionUID=1L;
+/**
+ * Thrown when 
+ * 
+ * @author Luke Senseney, Nikola Istvanic
+ *
+ */
+public class NotEvaluatable extends Exception {
+	private static final long serialVersionUID = 1L;
 
-	public NotEvaluatable()
-	{
+	/**
+	 * NotEvaluatable exception constructor which requires no parameters.
+	 */
+	public NotEvaluatable() {
 	}
 
-	public NotEvaluatable(String arg0)
-	{
+	/**
+	 * 
+	 * @param arg0
+	 */
+	public NotEvaluatable(String arg0) {
 		super(arg0);
 	}
 
-	public NotEvaluatable(Throwable arg0)
-	{
+	/**
+	 * 
+	 * @param arg0
+	 */
+	public NotEvaluatable(Throwable arg0) {
 		super(arg0);
 	}
 
-	public NotEvaluatable(String arg0,Throwable arg1)
-	{
-		super(arg0,arg1);
+	/**
+	 * 
+	 * @param arg0
+	 * @param arg1
+	 */
+	public NotEvaluatable(String arg0, Throwable arg1) {
+		super(arg0, arg1);
 	}
 
-	public NotEvaluatable(String arg0,Throwable arg1,boolean arg2,boolean arg3)
-	{
-		super(arg0,arg1,arg2,arg3);
+	/**
+	 * 
+	 * @param arg0
+	 * @param arg1
+	 * @param arg2
+	 * @param arg3
+	 */
+	public NotEvaluatable(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
 	}
 }
