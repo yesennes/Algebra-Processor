3744545a57fc4c95828a1b1b375f2d8be59da814
Nikola Istvanic <istvanic.nikola@gmail.com>
2015-12-15 11:51:24 -0500

    Checkstyle SystemEquations.java

--- Algebra Processor/src/lang/SystemEquations.java
+++ Algebra Processor/src/lang/SystemEquations.java
@@ -6,22 +6,20 @@ import java.util.ArrayList;
  * Unused
  * @author Luke Senseney
  */
-public class SystemEquations
-{// Start class
-	private ArrayList<Expression> equations=new ArrayList<Expression>();
-	@SuppressWarnings("unused") private ArrayList<Solution> solutions=new ArrayList<Solution>();
+public class SystemEquations { // Start class
+	private ArrayList<Expression> equations = new ArrayList<>();
+	
+	@SuppressWarnings("unused") 
+	private ArrayList<Solution> solutions = new ArrayList<>();
 
-	public SystemEquations()
-	{
+	public SystemEquations() {
 	}
 
-	public SystemEquations(String firstEquation)
-	{
+	public SystemEquations(String firstEquation) {
 		this.addEquation(firstEquation);
 	}
 
-	public void addEquation(String newEquation)
-	{// Start addEquation
+	public void addEquation(String newEquation) { // Start addEquation
 		equations.add(new Expression(newEquation));
 	}
-}// Glory to God
+}
